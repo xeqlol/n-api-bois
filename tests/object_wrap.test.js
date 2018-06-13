ccf5dedb0f3af623be95ebd31e0237137a6ea799
xeqlol <xeqlol@apri-code.ru>
2018-06-13 13:50:22 +0500

    Fixed

--- tests/object_wrap.test.js
+++ tests/object_wrap.test.js
@@ -2,8 +2,6 @@ const binding = require("../build/Release/object_wrap");
 
 describe("object wrap bindings", () => {
   it(`works fine`, () => {
-    /* console.log(new binding.MyObject(9));
-    
     const obj = new binding.TestObject(9);
     expect(obj.value).toEquals(9);
 
@@ -18,7 +16,6 @@ describe("object wrap bindings", () => {
     expect(obj.mulpitly(10).value).toEquals(130);
 
     const newObj = obj.mulpitly(-1);
-    expect(newObj.value).toEquals(-13); 
-    */
+    expect(newObj.value).toEquals(-13);
   });
 });
