9ce22cc48bf9e85e263bea57cca34364847a3aaa
xeqlol <xeqlol@apri-code.ru>
2018-06-13 13:58:39 +0500

    Finally works

--- tests/object_wrap.test.js
+++ tests/object_wrap.test.js
@@ -2,20 +2,20 @@ const binding = require("../build/Release/object_wrap");
 
 describe("object wrap bindings", () => {
   it(`works fine`, () => {
-    const obj = new binding.TestObject(9);
-    expect(obj.value).toEquals(9);
+    const obj = new binding.MyObject(9);
+    expect(obj.value).toEqual(9);
 
     obj.value = 10;
-    expect(obj.value).toEquals(10);
+    expect(obj.value).toEqual(10);
 
-    expect(obj.plusOne()).toEquals(11);
-    expect(obj.plusOne()).toEquals(12);
-    expect(obj.plusOne()).toEquals(13);
+    expect(obj.plusOne()).toEqual(11);
+    expect(obj.plusOne()).toEqual(12);
+    expect(obj.plusOne()).toEqual(13);
 
-    expect(obj.mulpitly().value).toEquals(13);
-    expect(obj.mulpitly(10).value).toEquals(130);
+    expect(obj.multiply().value).toEqual(13);
+    expect(obj.multiply(10).value).toEqual(130);
 
-    const newObj = obj.mulpitly(-1);
-    expect(newObj.value).toEquals(-13);
+    const newObj = obj.multiply(-1);
+    expect(newObj.value).toEqual(-13);
   });
 });
